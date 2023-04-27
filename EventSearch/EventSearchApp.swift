b9fc202a3ba48d5c6203d07b9e6aad3c0bae6831
Swapnil Chhatre <code.schhatre@gmail.com>
2023-04-27 12:07:22 -0700

    Initial Commit

---
+++ EventSearch/EventSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventSearchApp.swift
+//  EventSearch
+//
+//  Created by Swapnil Chhatre on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EventSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
